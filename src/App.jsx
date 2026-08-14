4ae19ea5e9348410a3453b53e78561716a5c1081
Aladin <myxcomputer92@gmail>
2026-08-14 21:37:49 +0500

    All errors are fixed which were inside App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
-import Center from './Components/Center'
 import Film from './Components/Film'
-// import Footer from './Components/Footer'
 import Jason from './Components/Jason'
 import Lucia from './Components/Lucia'
 
@@ -18,11 +14,9 @@ function App() {
     <>
     <Navbar/>
     <Hero/>
-    {/* <Center/> */}
     <Jason/>
     <Lucia/>
     <Film/>
-    {/* <Footer/> */}
       </>
   )
 }
